0325895f2d203b24be0bb546962ee1d5f87dd6ea
ZsoltJuhasz <juhasz.zsolt98@gmail.com>
2021-12-13 11:27:18 +0100

    Init

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Button} from 'react-native';
+import {useState} from 'react';
+
 
 export default function App() {
+
+  function renderItem({item}){
+    return(
+    <View>
+      <Text>{item.name} {item.salary}</Text>
+    </View>
+    );
+  }
+
+  const [employees, setEmployees] = useState([]);
+  const url = 'http://localhost:3000/employees';
+  function handleFetchButton(){
+    fetch(url)
+    //névtelen függvénynek a paramétere a response
+    .then(response => response.json()) 
+    .then(result => {
+      console.log(result);
+      setEmployees(result); 
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Lista</Text>
+      <Button title="Letölt" onPress={handleFetchButton}/>
+      <FlatList
+        data={employees}
+        renderItem={renderItem}
+        keyExtractor={item => item.name}  //ide az azonosító kellene csak mi olyat nem csináltunk
+      />
       <StatusBar style="auto" />
     </View>
   );
